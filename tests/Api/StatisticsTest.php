8a3dddc919c2a0858e698425b77ca6c45b43bc41
Alex K <alexander.katkov@outlook.com>
2021-11-23 20:01:47 +0300

    minor test data adjustment and removed useless var

--- tests/Api/StatisticsTest.php
+++ tests/Api/StatisticsTest.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Api;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
-use App\Entity\Url;
 use App\Tests\AliceFixtureDependentTestCase;
 use App\Tests\Traits\DatabasePurger;
 use App\Tests\Traits\GetContainerInstance;
@@ -35,7 +33,7 @@ class StatisticsTest extends AliceFixtureDependentTestCase
 
     public function testGetStatistics(): void
     {
-        $response = $this->client->request('GET', self::API_URL, [
+        $this->client->request('GET', self::API_URL, [
             'headers' => [
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json',
